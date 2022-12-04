d0d25c18e852f4792e7d725ded56d6fe5a2d1c63
John <johnwu2613@gmail.com>
2022-12-04 23:23:49 +0800

    Day 4

--- app/concepts/AOC2022/answers/day_4.rb
+++ app/concepts/AOC2022/answers/day_4.rb
@@ -1,9 +1,26 @@
 class AOC2022::Answers::Day4
   class << self
+    def data
+      read_file(4)
+        .split("\n")
+        .map { |s| s.split(",") }
+        .map { |(a, b)| [a.split("-").map(&:to_i), b.split("-").map(&:to_i)] }
+    end
+
     def ans_1
+      data.select do |((s1, e1), (s2, e2))|
+        range_a = s1..e1
+        range_b = s2..e2
+        range_a.cover?(range_b) || range_b.cover?(range_a)
+      end.size
     end
 
     def ans_2
+      data.select do |((s1, e1), (s2, e2))|
+        range_a = s1..e1
+        range_b = s2..e2
+        range_a.overlaps?(range_b)
+      end.size
     end
   end
 end
