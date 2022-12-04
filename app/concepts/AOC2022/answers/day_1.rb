062fef757c69b718eb9008e596b563c3a9a1542d
John <johnwu2613@gmail.com>
2022-12-04 17:23:56 +0800

    Day 1

--- app/concepts/AOC2022/answers/day_1.rb
+++ app/concepts/AOC2022/answers/day_1.rb
@@ -1,9 +1,18 @@
 class AOC2022::Answers::Day1
   class << self
+    def data
+      read_file(1)
+        .split("\n\n")
+        .map { |l| l.split("\n") }
+        .map { |data| data.map(&:to_i) }
+    end
+
     def ans_1
+      data.map(&:sum).max
     end
 
     def ans_2
+      data.map(&:sum).max(3).sum
     end
   end
 end
